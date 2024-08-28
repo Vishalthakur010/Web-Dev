562b6bd8a958b179bb5cf55fe41430a78d0ac0a2
vishal thakur <vishalthakur970820@gmail.com>
2024-08-28 22:27:31 +0530

    js class 1

---
+++ Javascript/68. Js class-1/index.js
@@ -0,0 +1,77 @@
+// Line below is used to print on the console window
+console.log('anyeonhaseyeong means hello');
+
+// let a='vishal';
+// console.log(a);
+
+// var b=50;
+// b=60;
+// console.log(b);
+
+// const num=40;
+// // num=10;
+// console.log(num);
+
+// let Name='vishal 01';
+// console.log(Name);
+
+// Name=500;
+// console.log(Name);
+
+// let age=25;
+// let status=(age>=18)? 'yes vote' : 'No vote';
+// console.log(status);
+
+// let marks=65;
+
+// if(marks>=90){
+//     console.log('A');
+// }
+// else if(marks>=80){
+//     console.log('B');
+// }
+// else if(marks>=70){
+//     console.log('C');
+// }
+// else if(marks>=60){
+//     console.log('D');
+// }
+// else{
+//     console.log('Fail')
+// }
+
+
+// Switch 
+// let num=4;
+
+// switch(num){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     case 4: console.log('D');
+//     break;
+//     default: console.log('not a valid number');
+// }
+
+// For loop
+// for(let i=1; i<=6;i++){
+//     console.log('vishal'+ i)
+// }
+
+// While Loop
+// let i=0
+// while(i<6){
+//     console.log('vishal'+ i);
+//     i++;
+// }
+
+// Do while Loop
+let a=1;
+ do{
+    console.log('hello'+ a);
+    a++;
+ }
+ while(a<10);
