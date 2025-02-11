61628a138de03aa0b6de3d25db31b1a879f1c4c0
vishal thakur <vishalthakur970820@gmail.com>
2025-02-11 08:33:11 +0530

    remove studynotion folder

--- STUDYNOTION/SERVER/routes/Contact.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { contactUs } = require('../controllers/Contactus')
-
-router.post('/contact', contactUs)
-
-module.exports = router
